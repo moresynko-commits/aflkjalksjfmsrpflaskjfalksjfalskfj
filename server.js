218e6996209a7b71cb7059a42d6f8636acfb5f1c
moresynko-commits <moresynko@gmail.com>
2026-03-23 15:05:12 -0500

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-require('http').createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('OK');
-}).listen(process.env.PORT || 3000);
-
-require('./index.js');
